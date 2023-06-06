78f4260dc1f9b83f0941350a0c973b91e0d066de
pepoJ34 <schoolpepoo@gmail.com>
2023-06-06 08:30:17 -0300

    construi e analisei balão

--- src/Balao.java
+++ src/Balao.java
@@ -1,5 +1,18 @@
 public class Balao {
 
+    //Atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
 
+    //Métodos
+    public String voar(){
+        return "voando";
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        //Construir balões
+        Balao b1 = new Balao();
+        String resultado = b1.voar();
+        System.out.println(resultado);
 
 
     }
