8050ba1833b7930b40c15f30a36493aa5080e1fa
Pedro Jeacomo <schoolpepoo@gmail.com>
2023-08-01 07:47:04 -0300

    Finalizando a atividade

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ public class Main {
         //Construir balões
         Scanner tcd = new Scanner(System.in);
 
-        /*BalaoAtv b1 = new BalaoAtv();
+        BalaoAtv b1 = new BalaoAtv();
         System.out.println("Insira número do Balão: ");
         b1.setNumeroBalao(tcd.next());
         System.out.println("Informe o registro de regularidade: ");
         b1.setRegistro(tcd.next());
+
+        BalaoAtv b2 = new BalaoAtv();
+        b2.setNumeroBalao("672");
+        b2.setRegistro("22314");
 
         Piloto p1 = new Piloto();
         System.out.println("Insira CPF do Piloto: ");
@@ -18,10 +22,15 @@ public class Main {
         System.out.println("Informe nome do Piloto: ");
         p1.setNome(tcd.next());
         System.out.println("Insira seu número de registro ANAC: ");
-        p1.setNumeroRegistro(tcd.next());*/
+        p1.setNumeroRegistro(tcd.next());
+
+        Piloto p2 = new Piloto();
+        p2.setCpf("222.777.444.55");
+        p2.setNome("Edimundo Augusto");
+        p2.setNumeroRegistro("787854");
 
         Pessoa pe1 = new Pessoa();
-        /*System.out.println("Informe o nome: ");
+        System.out.println("Informe o nome: ");
         pe1.setNome(tcd.next());
         System.out.println("Insira o CPF: ");
         pe1.setCpf(tcd.next());
@@ -30,15 +39,23 @@ public class Main {
         System.out.println("Informe o telefone: ");
         pe1.setTelefone(tcd.next());
         System.out.println("Informe o número de emergência: ");
-        pe1.setEmergencia(tcd.next());*/
-        System.out.println("A pessoa assinou o termo? (s/n)");
+        pe1.setEmergencia(tcd.next());
+        System.out.println("A pessoa assinou o termo? (true/false)");
         pe1.setAssinaturaTermo(tcd.nextBoolean());
         System.out.println("Qual o endereço?: ");
         pe1.setEndereco(tcd.next());
 
-        System.out.println(pe1.getAssinaturaTermo());
-        /*String resultado = b1.voar();
-        System.out.println(resultado);*/
+        Pessoa pe2 = new Pessoa();
+        pe2.setNome("Georgia Sousa");
+        pe2.setCpf("676.990.333.45");
+        pe2.setRg("56.877.467-X");
+        pe2.setTelefone("11 77788-9999");
+        pe2.setEmergencia("71 23334-0092");
+        pe2.setAssinaturaTermo(true);
+        pe2.setEndereco("Alameda Santos, 543, São Paulo - SP");
+
+        String resultado = b1.voar();
+        System.out.println(resultado);
 
 
     }
